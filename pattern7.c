b4a5fcfbab2f265485db6dac3d08f94a34774bee
AshwinSubramanyaGS <ashwin.subramanya@gmail.com>
2024-12-07 12:57:02 +0530

    patt 8

--- pattern7.c
+++ pattern7.c
@@ -3,3 +3,18 @@
 	5 4 3 
 	5 4 3 2
 	5 4 3 2 1*/
+#include<stdio.h>
+
+int main()
+{
+	int n=0;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			printf("%d ",j+1);
+		}
+		printf("\n");
+	}
+}
